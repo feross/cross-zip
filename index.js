3529e8b5bda57c1daec41e9ed4d247212d5db8ec
Feross Aboukhadijeh <feross@feross.org>
2016-05-02 22:36:17 +0200

    windows: remove existing zip folder

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ var cp = require('child_process')
 var fs = require('fs')
 var os = require('os')
 var path = require('path')
+var rimraf = require('rimraf')
 
 function zip (inPath, outPath, cb) {
-  // Windows zip command cannot zip files, only directories. So move the file into
-  // a temporary directory before zipping.
   if (process.platform === 'win32') {
     fs.stat(inPath, function (err, stats) {
       if (err) return cb(err)
@@ -26,12 +25,8 @@ function zip (inPath, outPath, cb) {
     doZip()
   }
 
-  function doZip () {
-    cp.exec(getZipCommand(inPath, outPath), function (err) {
-      cb(err)
-    })
-  }
-
+  // Windows zip command cannot zip files, only directories. So move the file into
+  // a temporary directory before zipping.
   function copyToTemp () {
     fs.readFile(inPath, function (err, inFile) {
       if (err) return cb(err)
@@ -46,15 +41,33 @@ function zip (inPath, outPath, cb) {
       })
     })
   }
+
+  // Windows zip command does not overwrite existing files. So do it manually first.
+  function doZip () {
+    if (process.platform === 'win32') {
+      rimraf(outPath, doZip2)
+    } else {
+      doZip2()
+    }
+  }
+
+  function doZip2 () {
+    cp.exec(getZipCommand(inPath, outPath), function (err) {
+      cb(err)
+    })
+  }
 }
 
 function zipSync (inPath, outPath) {
-  if (process.platform === 'win32' && fs.statSync(inPath).isFile()) {
-    var inFile = fs.readFileSync(inPath)
-    var tmpPath = path.join(os.tmpdir(), 'cross-zip-' + Date.now())
-    fs.mkdirSync(tmpPath)
-    fs.writeFileSync(path.join(tmpPath, path.basename(inPath)), inFile)
-    inPath = tmpPath
+  if (process.platform === 'win32') {
+    if (fs.statSync(inPath).isFile()) {
+      var inFile = fs.readFileSync(inPath)
+      var tmpPath = path.join(os.tmpdir(), 'cross-zip-' + Date.now())
+      fs.mkdirSync(tmpPath)
+      fs.writeFileSync(path.join(tmpPath, path.basename(inPath)), inFile)
+      inPath = tmpPath
+    }
+    rimraf.sync(outPath)
   }
   cp.execSync(getZipCommand(inPath, outPath))
 }
